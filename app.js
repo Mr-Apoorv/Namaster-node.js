bad5e14f9a4fc2add65aabede27ab88c1b907ffc
Mr-Apoorv <dixit.a.apoorv@gmail.com>
2026-09-20 15:11:25 +0530

    Namaste Node.js, Season 1, #E-4 : Common js modules.

---
+++ app.js
@@ -0,0 +1,19 @@
+require("./xyz");
+
+let {x, calculateSum} = require("./sum");
+
+
+var name = "Namaste NodeJs";
+
+var a = 10;
+var b = 20;
+
+// console.log(name);
+// console.log(a + b);
+
+// console.log(global);
+// console.log(this);
+
+calculateSum(a, b);
+
+console.log(x);
---
+++ sum.js
@@ -0,0 +1,11 @@
+
+
+let x = "hello world from sum"
+
+function calculateSum(a, b){
+    let sum = a+b;
+
+    console.log(sum);
+}
+
+module.exports = {x, calculateSum};
